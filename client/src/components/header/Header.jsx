603e08675b961e1def661bdbd589671d4e010c51
Velislav21 <zirovcho34@gmail.com>
2025-04-06 14:02:14 +0300

    adding the header

---
+++ client/src/components/header/Header.jsx
@@ -0,0 +1,24 @@
+import { Link } from "react-router"
+
+import styles from "./Header.module.css"
+
+
+export default function Header() {
+    return (
+        <header className={styles["header"]}>
+            <Link to="#" className={styles["site-title"]}>Crypto Currencies</Link>
+            <ul className={styles["links-container"]}>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">About</Link></li>
+                <li><Link to="#">Unknown</Link></li>
+            </ul>
+            <div className={styles["select-menu-container"]}>
+                <select name="currency-menu">
+                    <option value="usd">USD</option>
+                    <option value="eur">EUR</option>
+                    <option value="bgn">BGN</option>
+                </select>
+            </div>
+        </header>
+    )
+}
