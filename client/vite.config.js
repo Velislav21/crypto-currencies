5ef4197d7d811b6c1de7d2a430af9d015591673f
Velislav21 <zirovcho34@gmail.com>
2025-04-08 08:59:44 +0300

    vite config update

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,6 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
+      port: process.env.PORT ? parseInt(process.env.PORT) : 4173,
+      host: true,
       '/api': {
         target: "https://api.coingecko.com/api/v3",
         changeOrigin: true,
